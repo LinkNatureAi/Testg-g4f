63765e5a709bf33a0d900fd2ee3846522638c321
SONU KOL <108781856+LinkNatureAi@users.noreply.github.com>
2023-11-05 07:27:59 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,19 @@
 import g4f, asyncio
 
 _providers = [
+    g4f.Provider.Bard,
+    g4f.Provider.Liaobots,
+    g4f.Provider.Raycast,
+    g4f.Provider.AItianhu,
+    g4f.Provider.ChatgptDemo,
+    g4f.Provider.ChatgptFree,
+    g4f.Provider.FreeGpt,
+    g4f.Provider.GPTalk,
+    g4f.Provider.Llama2,
+	
+    g4f.Provider.GeekGpt,
+    g4f.Provider.Myshell,
+    g4f.Provider.ChatgptAi,
     g4f.Provider.Aichat,
     g4f.Provider.ChatBase,
     g4f.Provider.Bing,
